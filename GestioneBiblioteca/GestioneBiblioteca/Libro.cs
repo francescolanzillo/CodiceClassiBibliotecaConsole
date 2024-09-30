4e55003da2918cad1130ec4eecad39b7eb85fd09
francescolanzillo <roky0731@gmail.com>
2024-09-30 13:45:14 +0200

    secondo salvataggio
    salvataggio vero

---
+++ GestioneBiblioteca/GestioneBiblioteca/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestioneBiblioteca
+{
+     class Libro
+    {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int Pagine { get; set; }
+
+        public decimal Prezzo { get; set; }
+
+        public int CodiceSegreto { get; }
+
+        public Libro() 
+        {
+            //questo blocco di codice viene eseguito 
+            //alla creazione (new) degli oggetti di questa classe
+            CodiceSegreto = 321;
+        }
+        public decimal PrezzoInDollari()
+        {
+            decimal prezzoConvertito = Prezzo * 0.89M;
+            return prezzoConvertito;
+        }
+
+    }
+}
---
+++ GestioneBiblioteca/GestioneBiblioteca/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GestioneBiblioteca;
+
+Console.WriteLine("Gesione biblioteca");
+
+Console.WriteLine("Inserimento libri");
+
+Console.Write("inserisci il titolo:");
+
+Libro[] biblioteca = new Libro[500];
+int indice = 0;
+
+bool continua= true;
+while(continua )
+{
+
+    //sintassi di creazione di un oggetto 
+    //da una classe
+Libro oggettoLibro = new Libro();
+
+    oggettoLibro.Titolo=Console.ReadLine();
+    Console.Write("inserisci l'autore");
+    oggettoLibro.Autore =Console.ReadLine();
+    Console.Write("inserisci il numero di pagine:");
+    oggettoLibro.Pagine= int.Parse(Console.ReadLine());
+    Console.Write("inserisci il prezzo");
+    oggettoLibro.Prezzo = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("il prezzo in dollari sarà:" + oggettoLibro.PrezzoInDollari());
+
+    biblioteca[indice] = oggettoLibro;
+    indice++;
+
+    Console.WriteLine("Vuoi inserire un altro libro (S/N)?");
+    string risposta= Console.ReadLine();
+    if (risposta == "N")
+    {
+        continua = false;
+    }
+}
+
+
